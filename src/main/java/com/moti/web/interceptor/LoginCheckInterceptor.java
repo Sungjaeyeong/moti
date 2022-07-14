247767bfc50852a5c26e98ab56f04a542f6413b1
Sungjaeyeong <wodud2587@gmail.com>
2022-07-15 02:12:32 +0900

    로그인 체크 인터셉터 생성

---
+++ src/main/java/com/moti/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,37 @@
+package com.moti.web.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moti.web.SessionConst;
+import com.moti.web.exceptionhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        log.info("로그인 체크 인터셉터 - requestURI: {}", requestURI);
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
+            response.setCharacterEncoding("utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            ErrorResult errorResult = new ErrorResult("401", "인증 필요");
+            response.getWriter().write(objectMapper.writeValueAsString(errorResult));
+            return false;
+        }
+
+        return true;
+    }
+}
