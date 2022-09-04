a3266350b84b06fd7fe6011f479f268dc0318304
Sungjaeyeong <wodud2587@gmail.com>
2022-09-05 01:23:54 +0900

    더미 데이터 테스트

---
+++ src/main/java/com/moti/InitDb.java
@@ -0,0 +1,94 @@
+package com.moti;
+
+import com.moti.domain.comment.Comment;
+import com.moti.domain.post.Post;
+import com.moti.domain.user.entity.Job;
+import com.moti.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+//    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        public void dbInit() {
+            User user = User.builder()
+                    .email("wodud@afd.com")
+                    .password("abcdef")
+                    .name("wodud")
+                    .job(Job.DEV)
+                    .build();
+
+            User user2 = User.builder()
+                    .email("wodud2@afd.com")
+                    .password("abcdef")
+                    .name("wodud2")
+                    .job(Job.DEV)
+                    .build();
+
+            Post post = Post.builder()
+                    .title("제목")
+                    .content("내용")
+                    .user(user)
+                    .build();
+
+            Comment comment1 = Comment.builder()
+                    .content("댓글1")
+                    .post(post)
+                    .user(user)
+                    .build();
+
+            Comment comment2 = Comment.builder()
+                    .content("댓글2")
+                    .post(post)
+                    .user(user)
+                    .build();
+
+            Comment comment3 = Comment.builder()
+                    .content("댓글3")
+                    .post(post)
+                    .user(user)
+                    .build();
+
+            Comment comment4 = Comment.builder()
+                    .content("댓글4")
+                    .post(post)
+                    .user(user2)
+                    .build();
+
+            em.persist(user);
+            em.persist(user2);
+            em.persist(post);
+
+            for (int i=0; i<30; i++) {
+                Comment comment = Comment.builder()
+                        .content("댓글" + i)
+                        .post(post)
+                        .user(user)
+                        .build();
+                em.persist(comment);
+            }
+//            em.persist(comment1);
+//            em.persist(comment2);
+//            em.persist(comment3);
+//            em.persist(comment4);
+        }
+    }
+}
